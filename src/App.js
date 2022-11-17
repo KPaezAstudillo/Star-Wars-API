102158270d15120c1e65db091f8386327fcaf5fe
KPaezAstudillo <karinapaezastudillo@gmail.com>
2022-11-17 18:09:51 -0300

    added comments

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect } from "react";
 import { getPeople, getCharacter, searchCharacter } from "./api/people";
 import './App.css';
 import Modal from "./components/Modal";
@@ -14,14 +14,18 @@ function App() {
   const [page, setPage] = useState(1)
   const [modal, setModal] = useState(false)
 
+
+  //fetch every time the page number changes
   useEffect(() => {
     getPeople(page).then(setPeople)
   }, [page]);
 
+  //fetch to show details every time the user clicks a character
   useEffect(() => {
     getCharacter(currentCharacter).then(setDetails)
   }, [currentCharacter])
 
+  //function to extract id from the character's details and display modal 
   const showDetails = (character) => {
     const id = Number(character.url.split('/').slice(-2)[0])
     console.log(Number(character.url.split('/').slice(-2)[0]))
@@ -30,7 +34,7 @@ function App() {
     setModal(true)
   }
 
-
+//function to extract value from input and assign that value to searchCharacter fetch
   const handleSearch = (event) => {
     event.preventDefault();
     const text = event.target.value;
@@ -38,6 +42,7 @@ function App() {
     searchCharacter(searchString).then(setPeople)
   }
 
+  //set new page number according to previous or next button
   const handlePage = (next) => {
     if (!people.previous && page + next <= 0) return;
     if (!people.next && page + next > 9) return;
@@ -53,6 +58,8 @@ function App() {
         <h1 className="mx-auto mt-3">Starwars Characters</h1>
         <h6 className="mx-auto mb-5">A guide from a galaxy far, far away </h6>
       </div>
+
+      {/* cards with characters names */}
       <div className="row d-flex">
         {people?.results?.map((character) =>
         (
@@ -61,10 +68,12 @@ function App() {
         }
       </div>
       <div className="d-flex">
-        <button className="btn btn-secondary mx-auto bg-warning text-black" onClick={() => handlePage(-1)}>Previous</button>
+        <button className="btn btn-secondary ms-auto me-5 bg-warning text-black" onClick={() => handlePage(-1)}>Previous</button>
         Page {page}
-        <button className="btn btn-secondary mx-auto bg-warning text-black" onClick={() => handlePage(1)}>Next</button>
+        <button className="btn btn-secondary me-auto ms-5 bg-warning text-black" onClick={() => handlePage(1)}>Next</button>
       </div>
+
+      {/* modal displayed when user clicks on character's card */}
       {details && modal && (
         <>
           <Modal name={details?.name} mass={details?.mass} height={details?.height} birth_year={details.birth_year} />
--- src/api/people.js
+++ src/api/people.js
@@ -1,15 +1,18 @@
+//this fetch function gets list of characters
 export async function getPeople(page){
     const response = await fetch(`https://swapi.dev/api/people/?page=${page}`);
     const data = await response.json();
     return data;
 }
 
+//this fetch function gets single character info
 export async function getCharacter(id){
     const response = await fetch(`https://swapi.dev/api/people/${id}`)
     const data = await response.json();
     return data;
 }
 
+//this fetch function searches for the user input
 export async function searchCharacter(string){
     const response = await fetch(`https://swapi.dev/api/people/?search=${string}`)
     const data = await response.json();
